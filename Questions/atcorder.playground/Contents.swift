fe9320f2e438be3e9e26ac696554b3227f8ec490
masa <mickey24352002@gmail.com>
2019-05-15 23:43:35 +0900

    add new file.

---
+++ Questions/atcorder.playground/Contents.swift
@@ -0,0 +1,63 @@
+import Cocoa
+
+//let input1 = 23, input2 = 5
+//
+//func judgeOdd(num1: Int, num2: Int) -> String {
+//
+//    let judgeNum = num1 * num2
+//    if judgeNum % 2 == 0 {
+//        return "Even"
+//    } else {
+//        return "Odd"
+//    }
+//
+//}
+//
+//let ans = judgeOdd(num1: input1, num2: input2)
+//
+//print(ans)
+
+
+//let input = 111010101010101001
+//
+//func getAns(num: Int) -> Int {
+//    var count = 0
+//
+//    let string = String(num)
+//
+//    string.forEach {
+//        if $0 == "1" {
+//            count += 1
+//        }
+//    }
+//
+//    return count
+//}
+//
+//let ans = getAns(num: input)
+//print(ans)
+
+
+//var input = [100,100,100]
+//
+//func getAns(array: inout [Int]) -> Int {
+//    var count = 0
+//    root: while true {
+//        for (i,v) in array.enumerated() {
+//            if v % 2 != 0 { break root }
+//            array[i] /= 2
+//        }
+//        count += 1
+//    }
+//    return count
+//}
+//
+//let ans = getAns(array: &input)
+//print(ans)
+
+//let a = 2, b = 2, c = 3, x = 100
+//
+//func getAns(a: Int, b: Int, c: Int, total: Int) -> Int {
+//
+//}
+
