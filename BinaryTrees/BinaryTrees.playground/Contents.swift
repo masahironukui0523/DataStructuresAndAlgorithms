5d3b84340910ec803a015756665b55a62082f9bf
masa <mickey24352002@gmail.com>
2019-06-08 14:06:27 +0900

    Update.

--- BinaryTrees/BinaryTrees.playground/Contents.swift
+++ BinaryTrees/BinaryTrees.playground/Contents.swift
@@ -10,13 +10,50 @@ class BinaryNode<Element> {
     }
 }
 
-struct Stack:
+extension BinaryNode: CustomStringConvertible {
+    public var description: String {
+        return diagram(for: self)
+    }
+    
+    private func diagram(for node: BinaryNode?,
+                         _ top: String = "",
+                         _ root: String = "",
+                         _ bottom: String = "") -> String {
+        guard let node = node else {
+            return root + "nil\n"
+        }
+        if node.leftChild == nil && node.rightChild == nil {
+            return root + "\(node.value)\n"
+        }
+        return diagram(for: node.rightChild,
+                       top + " ", top + "┌──", top + "│ ")
+            + root + "\(node.value)\n"
+            + diagram(for: node.leftChild,
+                      bottom + "│ ", bottom + "└──", bottom + " ")
+    }
+}
+
+
+//struct Stack:
 
 extension BinaryNode {
     func traverseInOrder(visit: (Element) -> Void) {
         leftChild?.traverseInOrder(visit: visit)
+        visit(value)
+        rightChild?.traverseInOrder(visit: visit)
+    }
+    func traversePostOrder(visit: (Element) -> Void) {
+        leftChild?.traversePostOrder(visit: visit)
+        rightChild?.traversePostOrder(visit: visit)
+        visit(value)
+    }
+    func traversePreOrder(visit: (Element) -> Void) {
+        visit(value)
+        leftChild?.traversePostOrder(visit: visit)
+        rightChild?.traversePostOrder(visit: visit)
     }
 }
+
 let ten = BinaryNode(10)
 let nine = BinaryNode(9)
 let two = BinaryNode(2)
@@ -31,3 +68,11 @@ nine.leftChild = one
 nine.rightChild = three
 two.leftChild = four
 two.rightChild = six
+
+print(ten.description)
+//ten.traverseInOrder {
+//    print($0)
+//}
+ten.traversePostOrder {
+    print($0)
+}
--- Questions/reverseInt.playground/Contents.swift
+++ Questions/reverseInt.playground/Contents.swift
@@ -1,6 +1,9 @@
 import Cocoa
 
+// sol 1
 var num = 4629
+var num2 = 593842709
+var num3 = 7204702570280572085
 
 func getAns(num: Int) -> Int {
     let count = String(num).count
@@ -12,5 +15,15 @@ func getAns(num: Int) -> Int {
     return ans
 }
 
-let ans = getAns(num: num)
+let ans = getAns(num: num3)
 print(ans)
+
+
+// sol 2
+func getAns2(num: Int) -> Int {
+    let str = String(num)
+    return Int(String(str.reversed()))!
+}
+
+let ans2 = getAns2(num: num3)
+print(ans2)
