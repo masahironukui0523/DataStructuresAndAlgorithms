be3febf4c5bf984b69e2d47678121d2045e6eedc
masa <mickey24352002@gmail.com>
2019-06-08 21:36:22 +0900

    Add max property to the stack.

--- Stack/Stack.playground/Pages/Playground.xcplaygroundpage/Contents.swift
+++ Stack/Stack.playground/Pages/Playground.xcplaygroundpage/Contents.swift
@@ -2,16 +2,33 @@
 import Foundation
 
 
-class Stack<Element> {
+class Stack<Element: Comparable> {
     
     var storage = [Element]()
     
+    var maxValue: Element? {
+        return subStorage.last
+    }
+    
     func push(element: Element) {
         storage.append(element)
+        print("----------------------------------------")
+        if let max = subStorage.last {
+            if max < element {
+                print("new element(\(element)) is larger than max(\(max))", subStorage)
+                subStorage.append(element)
+            } else {
+                print("max(\(max)) is larger than new element(\(element))", subStorage)
+                subStorage.append(max)
+            }
+        } else {
+            subStorage.append(element)
+        }
     }
     
     func pop() {
         storage.popLast()
+        subStorage.popLast()
     }
     
     func peek() -> Element? {
@@ -24,16 +41,36 @@ class Stack<Element> {
     
     init (_ elements: [Element]) {
         self.storage = elements
+        
+        // sort関数を使わずにソートする
+        guard var max = elements.first else { return }
+        elements.forEach {
+            if max < $0 {
+                max = $0
+                self.subStorage.append($0)
+            } else {
+                self.subStorage.append(max)
+            }
+        }
     }
+    
+    // original
+    private var subStorage = [Element]()
+    
 }
 
-let array = [34,52,52,5,62]
+
+let array = [4,13,3,5,4]
+//let array = [Int]()
 
 let stack = Stack(array)
-stack.push(element: 23)
-
+//stack.push(element: 23)
+stack.push(element: 4)
+stack.push(element: 2)
+stack.push(element: 5)
+stack.push(element: 14)
+stack.push(element: 1)
+stack.push(element: 15)
 stack.pop()
-print(stack.storage)
 
-stack.peek()
-stack.isEmpty()
+print(stack.maxValue)
